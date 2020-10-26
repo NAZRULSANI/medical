95c179fbd3be06498bfa707035f3ac9051fcf618
rizwanNarsun <rizwan.ahmed@narsun.pk>
2020-10-27 00:20:00 +0500

    2nd push

--- App.js
+++ App.js
@@ -96,3 +96,228 @@ function HomeScreen({ navigation }) {
     </ImageBackground>
   );
 }
+
+function FirstAid({ navigation }) {
+  return (
+    <ImageBackground
+      style={{ flex: 1, width: "100%" }}
+      source={require("./app/images/background.jpg")}
+    >
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: 50,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 50,
+            fontWeight: "bold",
+            textAlign: "center",
+          }}
+        >
+          First Aid
+        </Text>
+        <Button
+          block
+          large
+          style={{ marginTop: 20, marginHorizontal: 30 }}
+          danger
+          rounded
+          onPress={() => navigation.navigate("Cut")}
+        >
+          <Text>Cut</Text>
+        </Button>
+        <Button
+          block
+          large
+          style={{ marginTop: 20, marginHorizontal: 30 }}
+          danger
+          rounded
+          onPress={() => navigation.navigate("Burn")}
+        >
+          <Text>Burn</Text>
+        </Button>
+        <Button
+          block
+          large
+          style={{ marginTop: 20, marginHorizontal: 30 }}
+          danger
+          rounded
+          onPress={() => navigation.navigate("CommonDisease")}
+        >
+          <Text>Common Disease</Text>
+        </Button>
+        <Button
+          rounded
+          block
+          large
+          iconLeft
+          style={{ marginTop: 100, marginHorizontal: 50 }}
+          info
+          onPress={() => navigation.navigate("Home")}
+        >
+          <Text>Home</Text>
+        </Button>
+      </View>
+    </ImageBackground>
+  );
+}
+
+function Cut({ navigation }) {
+  return (
+    <ImageBackground
+      style={{ flex: 1, width: "100%" }}
+      source={require("./app/images/background.jpg")}
+    >
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: 50,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 50,
+            fontWeight: "bold",
+            textAlign: "center",
+          }}
+        >
+          Cut
+        </Text>
+        <Text style={{ paddingHorizontal: 20 }}>
+          Wash your hands with soap and water. Wash the cut to prevent
+          infection. Use cool or lukewarm water and a mild soap or cleanser to
+          gently remove dirt or debris. Stop the bleeding. Apply pressure to the
+          cut using a clean washcloth or gauze. Maintain pressure for one to two
+          minutes or until the bleeding stops. Apply petroleum jelly. This will
+          help keep the wound moist for faster healing. Make sure you apply it
+          continuously until the cut heals. To help prevent the spread of dirt
+          and bacteria, consider using petroleum jelly from a tube instead of a
+          jar. Do not apply topical antibiotics. Cover the cut with a sterile
+          bandage. This will help protect the cut and prevent it from reopening.
+          Change the bandage daily, and keep the cut covered until it heals.
+          Consider taking over-the-counter pain medication. Acetaminophen can
+          help relieve painful cuts. Make sure your tetanus vaccination is up to
+          date if your cut is from a dirty or rusty object. If you aren’t sure,
+          contact your primary care doctor.
+        </Text>
+        <Button
+          rounded
+          block
+          large
+          iconLeft
+          style={{ marginTop: 100, marginHorizontal: 50 }}
+          info
+          onPress={() => navigation.navigate("Home")}
+        >
+          <Text>Home</Text>
+        </Button>
+      </View>
+    </ImageBackground>
+  );
+}
+
+function Burn({ navigation }) {
+  return (
+    <ImageBackground
+      style={{ flex: 1, width: "100%" }}
+      source={require("./app/images/background.jpg")}
+    >
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: 50,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 50,
+            fontWeight: "bold",
+            textAlign: "center",
+          }}
+        >
+          Burn
+        </Text>
+        <Text style={{ paddingHorizontal: 20 }}>
+          Most minor burns can be treated at home. They usually heal within a
+          couple of weeks. For serious burns, after appropriate first aid and
+          wound assessment, your treatment may involve medications, wound
+          dressings, therapy and surgery. The goals of treatment are to control
+          pain, remove dead tissue, prevent infection, reduce scarring risk and
+          regain function. People with severe burns may require treatment at
+          specialized burn centers. They may need skin grafts to cover large
+          wounds. And they may need emotional support and months of follow-up
+          care, such as physical therapy.
+        </Text>
+        <Button
+          rounded
+          block
+          large
+          iconLeft
+          style={{ marginTop: 100, marginHorizontal: 50 }}
+          info
+          onPress={() => navigation.navigate("Home")}
+        >
+          <Text>Home</Text>
+        </Button>
+      </View>
+    </ImageBackground>
+  );
+}
+
+function CommonDisease({ navigation }) {
+  return (
+    <ImageBackground
+      style={{ flex: 1, width: "100%" }}
+      source={require("./app/images/background.jpg")}
+    >
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: 50,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 50,
+            fontWeight: "bold",
+            textAlign: "center",
+          }}
+        >
+          Common Disease
+        </Text>
+        <Text style={{ paddingHorizontal: 20 }}>
+          Modern medicine needs new kinds of antibiotics and antivirals to treat
+          drug-resistant infections. But the pipeline of new drugs is drying up.
+          For example, nearly 40 years elapsed between introduction of the two
+          newest molecular classes of antibiotics: fluoroquinolones (such as
+          Cipro) in 1962 and the oxazolidinones (such as Zyvox) in 2000. Major
+          pharmaceutical companies have limited interest in dedicating resources
+          to the antibiotics market because these short-course drugs are not as
+          profitable as drugs that treat chronic conditions and
+          lifestyle-related ailments, such as high blood pressure or high
+          cholesterol. Antibiotic research and development is also expensive,
+          risky, and time consuming. Return on that investment can be
+          unpredictable, considering that resistance to antibiotics develops
+          over time, eventually making them less effective.
+        </Text>
+        <Button
+          rounded
+          block
+          large
+          iconLeft
+          style={{ marginTop: 100, marginHorizontal: 50 }}
+          info
+          onPress={() => navigation.navigate("Home")}
+        >
+          <Text>Home</Text>
+        </Button>
+      </View>
+    </ImageBackground>
+  );
+}
